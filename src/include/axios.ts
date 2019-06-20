f648e465ed6e8584fa6e1565f832c6bacc7e5dc3
rwalkercodes <walker.ryan182@gmail.com>
2019-06-20 09:21:47 -0500

    deleted unneeded axios.ts file from branch

--- src/include/axios.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export const project2_api = axios.create({
-   baseURL: 'http://localhost:8888',
-   headers:{
-       'Content-Type': 'application/json'
-   },
-   withCredentials: true
-})
