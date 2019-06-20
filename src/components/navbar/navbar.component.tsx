ddde4f5b63fa388f08f6f33d5116665d2586ca08
rwalkercodes <walker.ryan182@gmail.com>
2019-06-20 09:48:49 -0500

    changed nav comp back to original master version

--- src/components/navbar/navbar.component.tsx
+++ src/components/navbar/navbar.component.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import pmsLogo from '../../assets/rev-logo.png'  
-import { SignUpButton } from './signup.component';
 
 export class NavBarComponent extends React.Component {
     render() {
@@ -18,11 +17,10 @@ export class NavBarComponent extends React.Component {
             <div className="collapse navbar-collapse" id="navbarsExample04">
               <ul className="navbar-nav ml-auto margin-nav">
                 <li className="nav-item active">
-                  <Link to="/home" className="unset-anchor nav-link">Login</Link>
+                  <Link to="/home" className="unset-anchor nav-link">Home</Link>
                 </li>
                 <li className="nav-item active">
-                  {/* <Link to="/login" className="unset-anchor nav-link"></Link> */}
-                  <SignUpButton/>
+                  <Link to="/login" className="unset-anchor nav-link">Log in</Link>
                 </li>
                 
                
