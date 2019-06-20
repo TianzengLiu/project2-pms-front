6f4c3ff291ccc937cd9c17693352f1ae08547043
rwalkercodes <walker.ryan182@gmail.com>
2019-06-20 09:47:19 -0500

    changed home comp back to original master version

--- src/components/home/home.component.tsx
+++ src/components/home/home.component.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import {NavBarComponent} from '../navbar/navbar.component'
-import LoginComponent from '../form/login.component'
 
 export class HomeComponent extends React.Component {
     render() {
         return (
             <div>
                 <NavBarComponent />
-                <LoginComponent/>
+                <h1> Welcome to Parking Managment System</h1>
+
+
 
             </div>
         )
